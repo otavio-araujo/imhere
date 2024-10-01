6c2ec239ea12c8a2a7ab4e5e82c74d634b201f3e
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-01 17:02:29 -0300

    Organizando o projeto

---
+++ src/screens/home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#131016",
+    flex: 1,
+  },
+
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+})
