8f8449d9c0d53f262f99daac2ea128f6d5e2f799
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-01 16:54:02 -0300

    Utilizando StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,28 +1,34 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#131016",
-        flex: 1,
-      }}
-    >
-      <Text
-        key="1"
-        style={{
-          color: "#FDFCFE",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginTop: 48,
-        }}
-      >
+    <View style={styles.container}>
+      <Text key="1" style={styles.eventName}>
         Nome do evento:
       </Text>
-      <Text key="2" style={{ color: "#6B6B6B", fontSize: 16 }}>
+      <Text key="2" style={styles.eventDate}>
         Segunda, 01 de outubro de 2024
       </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#131016",
+    flex: 1,
+  },
+
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+})
