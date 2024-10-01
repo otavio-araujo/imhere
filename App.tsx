9f67d5e7f53af432148585e333a9e94f04f46162
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-01 00:31:20 -0300

    Estilizando elementos.

--- App.tsx
+++ App.tsx
@@ -3,9 +3,26 @@ import { Text, View } from "react-native"
 
 export default function App() {
   return (
-    <View>
-      <Text key="1">React</Text>
-      <Text key="1">Native</Text>
+    <View
+      style={{
+        backgroundColor: "#131016",
+        flex: 1,
+      }}
+    >
+      <Text
+        key="1"
+        style={{
+          color: "#FDFCFE",
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 48,
+        }}
+      >
+        Nome do evento:
+      </Text>
+      <Text key="2" style={{ color: "#6B6B6B", fontSize: 16 }}>
+        Segunda, 01 de outubro de 2024
+      </Text>
     </View>
   )
 }
